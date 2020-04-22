edc9ffb2821821a476300062f3941b14e70086b8
Mitch Gavan <mitchgavan@gmail.com>
2020-04-22 17:46:04 +1000

    feat: set up babel and dev server

--- webpack.config.js
+++ webpack.config.js
@@ -4,14 +4,29 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'React MDX Style Guide',
+      template: 'src/index.html',
     }),
   ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
   },
 };
